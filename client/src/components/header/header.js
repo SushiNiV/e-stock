499bb394c537da0f47735ca4c2906b4b2357e2be
SushiNiV <sha.gmdev@gmail.com>
2025-04-23 18:32:17 +0800

    Initial Commit

---
+++ client/src/components/header/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <div className="header-left">
+        <button className="menu-btn">
+          <FontAwesomeIcon icon={faBars} className="menu-icon" />
+        </button>
+        <div className="logo-group">
+            <img src="/es-logo-trnsc.png" alt="EaseStock Logo" className="logo-image" />
+            <h1 className="logo">
+                <span className="e">Ease</span><span className="s">-Stock</span>
+            </h1>
+        </div>
+      </div>
+
+      <div className="header-right">
+        <span className="user-info">Welcome, Admin</span>
+        <button className="logout-btn">Logout</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
