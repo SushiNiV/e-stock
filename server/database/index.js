b1e75d24ce6c79b62a9233b4360c54a56ac5aacb
SushiNiV <sha.gmdev@gmail.com>
2025-05-07 17:12:06 +0800

    Index Backend

---
+++ server/database/index.js
@@ -0,0 +1,24 @@
+const regLogServer = require('./reg-log');
+const regRoleServer = require('./reg-role');
+const categoryServer = require('./categ-db');
+const prodDbServer = require('./prod-db'); 
+
+const regLogPort = 3001;
+regLogServer.listen(regLogPort, () => {
+  console.log(`reg-log server running on http://localhost:${regLogPort}`);
+});
+
+const regRolePort = 3002;
+regRoleServer.listen(regRolePort, () => {
+  console.log(`reg-role server running on http://localhost:${regRolePort}`);
+});
+
+const categoryPort = 3003;
+categoryServer.listen(categoryPort, () => {
+  console.log(`category-service running on http://localhost:${categoryPort}`);
+});
+
+const prodDbPort = 3004; 
+prodDbServer.listen(prodDbPort, () => {
+  console.log(`product-service running on http://localhost:${prodDbPort}`);
+});
